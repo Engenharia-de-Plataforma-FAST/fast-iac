6d50e459d191476cc1c4464401576186247a2f25
Krix Apolinário <krixapolinario@gmail.com>
2024-11-01 08:44:42 -0300

    creating ssh key

--- lab-terraform-ansible/terraform/outputs.tf
+++ lab-terraform-ansible/terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "jenkins_url" {
-  description = "URL do Jenkins"
-  value = "http://${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip}:8080/"
-}
+# output "jenkins_url" {
+#   description = "URL do Jenkins"
+#   value = "http://${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip}:8080/"
+# }
 
-output "sonarqube_url" {
-  description = "URL do Sonarqube"
-  value = "http://${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip}:9000/"
-}
+# output "sonarqube_url" {
+#   description = "URL do Sonarqube"
+#   value = "http://${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip}:9000/"
+# }
